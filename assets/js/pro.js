dd6e4edba942b07b694b6d14f666d11c87e38906
asadbek <asadbekqulboyev@gmail.com>
2025-02-27 08:13:38 +0500

    Advantages section add

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -1,7 +1,32 @@
 $(document).ready(function(){
     // header
+    
     $('.catalog').click(function () { 
         $(this).toggleClass('active');
         $(this).next('.subcatalog').slideToggle();
     });
+    // hero
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplaySpeed: 3000
+    });
+    $('.prev').click(function(){
+        $('.slider').slick('slickPrev');
+    });
+    
+    $('.next').click(function(){
+        $('.slider').slick('slickNext');
+    });
 })
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 5) {
+        $('.header').addClass('active');
+    }else{
+        $('.header').removeClass('active');
+    }
+});
