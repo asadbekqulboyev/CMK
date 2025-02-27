6fc58ae01dbd9b0f588ace22ac2b142c940d1a2e
asadbek <asadbekqulboyev@gmail.com>
2025-02-27 12:45:36 +0500

    Finish 50%

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -22,6 +22,36 @@ $(document).ready(function(){
     $('.next').click(function(){
         $('.slider').slick('slickNext');
     });
+    // promotions
+    $('.promotions_slider').slick({
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplaySpeed: 3000
+    });
+    $('.promotions_button .prev').click(function(){
+        $('.promotions_slider').slick('slickPrev');
+    });
+    
+    $('.promotions_button .next').click(function(){
+        $('.promotions_slider').slick('slickNext');
+    });
+    // about
+    $('.slide_about').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        autoplaySpeed: 3000
+    });
+    $('.promotions_button .prev').click(function(){
+        $('.promotions_slider').slick('slickPrev');
+    });
+    
+    $('.promotions_button .next').click(function(){
+        $('.promotions_slider').slick('slickNext');
+    });
 })
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 5) {
