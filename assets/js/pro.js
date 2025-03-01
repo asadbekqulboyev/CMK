4fee91a518bf621b28105b98fb0ca5c7b7acf723
asadbek <asadbekqulboyev@gmail.com>
2025-03-01 10:53:32 +0500

    Finish 2page

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -22,6 +22,9 @@ $(document).ready(function(){
     $('.next').click(function(){
         $('.slider').slick('slickNext');
     });
+    $('.slider').on('afterChange', function(event, slick, currentSlide){
+        $('#current_slide').text(currentSlide + 1);
+    });
     // promotions
     $('.promotions_slider').slick({
         infinite: true,
@@ -41,17 +44,41 @@ $(document).ready(function(){
     // about
     $('.slide_about').slick({
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         arrows: false,
-        autoplaySpeed: 3000
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1120, // Ekran 1120px dan kichik bo‘lsa
+              settings: {
+                slidesToShow: 1 // 1 ta slayd ko‘rsatish
+              }
+            }
+          ]
     });
-    $('.promotions_button .prev').click(function(){
-        $('.promotions_slider').slick('slickPrev');
+
+    $('.btn_slideto.prev').click(function(){
+        $('.slide_about').slick('slickPrev');
     });
     
-    $('.promotions_button .next').click(function(){
-        $('.promotions_slider').slick('slickNext');
+    $('.btn_slideto.next').click(function(){
+        $('.slide_about').slick('slickNext');
     });
+    // faq
+    $(".faq_header").on("click", function () {
+        let item = $(this).parent(); 
+        let body = $(this).next(".faq_description");
+        // $(".faq_description").slideUp()
+        // $(".faq_description:first").slideDown()
+        if (item.hasClass("active")) {
+            item.removeClass("active");
+        } else {
+            $(".faq_item").removeClass("active"); 
+            item.addClass("active");
+        }
+    });
+    // forms
+    $("input[type=tel]").inputmask("+7 (999)-999-99-99");
 })
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 5) {
